d62b93e5ced5424984e2f761cb770f89cc320875
bogdanm <cwdw@icloud.com>
2025-11-21 09:39:53 +0300

    first

---
+++ Makefile
@@ -0,0 +1,8 @@
+install:
+		npm ci
+		chmod +x bin/brain-games.js.
+
+brain-games:
+			node bin/brain-games.js
+
+
---
+++ bin/brain-games.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+import welcomeUser from '../src/cli.js'
+
+console.log('Welcome to the Brain Games!')
+
+const name = welcomeUser()
+
+console.log(`Hello, ${name}!`)
